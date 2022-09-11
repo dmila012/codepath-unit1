42110007df255fd6bea2e6e4c1fac4abca4996f1
dmila012 <dmila012@fiu.edu>
2022-09-11 15:36:58 -0400

    Added Flix App files to repository

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Daniel Milanes  on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
