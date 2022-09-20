edf850990189dbed4ec36d75d8276559ce2efd6f
dmila012 <dmila012@fiu.edu>
2022-09-20 19:18:10 -0400

    Flix-Part 2 Added
    Updated App to reflex part 2 of the assignment and updated Read Me.

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Daniel Milanes  on 9/20/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
